e1fe5d8768292abfea8dcf76d5b595f587ebdd58
Jaehoon Choi <plaintext@andromedarabbit.net>
2017-02-23 00:00:44 +0900

    FontAwesome 을 이용해 일부 메뉴에 아이콘 적용

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Nav, Navbar, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+import TopNav from './TopNav';
 
 class App extends Component {
   render() {
     return (
-        <Navbar>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <a href="#">Everwriter</a>
-                </Navbar.Brand>
-            </Navbar.Header>
-            <Nav pullRight="true">
-                <NavItem eventKey={1} href="#">Sync</NavItem>
-                <NavDropdown eventKey={3} title="Profile" id="basic-nav-dropdown">
-                    <MenuItem eventKey={3.1}>Settings</MenuItem>
-                    <MenuItem eventKey={3.2}>Exports as...</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey={3.3}>Sign out</MenuItem>
-                </NavDropdown>
-            </Nav>
-        </Navbar>
+
+        <TopNav/>
     );
   }
 }
---
+++ src/TopNav.js
@@ -0,0 +1,32 @@
+/**
+ * Created by tywin on 22/02/2017.
+ */
+import React, { Component } from 'react';
+import './App.css';
+import { Nav, Navbar, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
+
+class TopNav extends Component {
+    render() {
+        return (
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">Everwriter</a>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Nav pullRight="true">
+                    <NavItem eventKey={1} href="#">Sync</NavItem>
+                    <NavDropdown eventKey={3} title="Profile" id="basic-nav-dropdown">
+                        <MenuItem eventKey={3.1}><FontAwesome name="cog"/> Settings</MenuItem>
+                        <MenuItem eventKey={3.2}><FontAwesome name="files-o"/> Exports as...</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey={3.3}><FontAwesome name="sign-out"/> Sign out</MenuItem>
+                    </NavDropdown>
+                </Nav>
+            </Navbar>
+        );
+    }
+}
+
+export default TopNav;
